6f5dabba16d2e2ff536f4cb0bf58b6403488834c
Chathuri Ranasinghe <120873491+chathuriranasinghe@users.noreply.github.com>
2024-09-08 03:44:49 +0530

    Finalized the website

--- main.js
+++ main.js
@@ -6,3 +6,26 @@ var typed = new Typed("#text", {
     loop: true,
 });
 
+function startProgress(element, endValue, speed) {
+    let progressStartValue = 0;
+    let progressValue = element.querySelector(".progress-value");
+    let circularProgress = element;
+
+    let progress = setInterval(() => {
+        progressStartValue++;
+
+        progressValue.textContent = `${progressStartValue}%`;
+        circularProgress.style.background = `conic-gradient(#0ef ${progressStartValue * 3.6}deg, #000000 0deg)`;
+
+        if (progressStartValue == endValue) {
+            clearInterval(progress);
+        }
+    }, speed);
+}
+
+let progressBars = document.querySelectorAll(".circular-progress");
+
+startProgress(progressBars[0], 90, 10);  
+startProgress(progressBars[1], 80, 10);  
+startProgress(progressBars[2], 75, 10);  
+startProgress(progressBars[3], 85, 10);  
